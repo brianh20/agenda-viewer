2fc548c8c8295adfd76e518176b6f228d5aacebc
unknown <brian.hume@live.com>
2017-05-31 02:44:11 -0300

    One bug to go!

--- src/app/services/contacts.service.js
+++ src/app/services/contacts.service.js
@@ -5,7 +5,7 @@ export const ContactsService = function ($http) {
         url: 'https://s3.amazonaws.com/technical-challenge/Contacts_v2.json',
         method: 'GET'
       }).then((res) => {
-      	// Return response
+        // Return response
         return res;
       }, () => {
         // Handle error
